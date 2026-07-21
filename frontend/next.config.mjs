4fe340ed0ef2c53b846147dc754d31e78ec1e7bf
ni-hal <nihalkoyamon93@gmail.com>
2026-07-21 23:38:03 +0530

    add-backendurl

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const apiBackendUrl = process.env.API_BACKEND_URL ?? "https://micro-ats-ft7d.vercel.app";
+const apiBackendUrl = process.env.API_BACKEND_URL ?? "https://micro-ats-jet.vercel.app";
 
 const nextConfig = {
   async rewrites() {
